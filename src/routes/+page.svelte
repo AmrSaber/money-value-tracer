14a5151b6542996dbb69f65e631e992eaeccc8ff
Amr Saber <amr.m.saber.mail@gmail.com>
2024-05-12 22:09:54 +0100

    add WIP UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>Work in progress...</h1>
+</main>
+
+<style>
+	main {
+		margin-inline: auto;
+		width: fit-content;
+		min-height: 100vh;
+
+		display: flex;
+		flex-direction: column;
+
+		align-items: center;
+		justify-content: center;
+	}
+</style>
